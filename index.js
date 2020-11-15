b3713f95b5022f75dbda9bfc2fc3926d7181dc80
Brad <bradjmeyn@gmail.com>
2020-11-16 08:22:16 +1100

    made responsibe for larger screens and changed style

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //input DOM selectors
 let startingAmount = document.querySelector("#startingAmount");
 let contribution = document.querySelector("#contribution");
@@ -8,21 +7,19 @@ let frequency = document.querySelector("#frequency");
 let button = document.querySelector("#button");
 
 let results = document.getElementById("results");
-let details = document.getElementById("details");
 
- let years = [];
-  let initial = [];
-  let contributions = [];
-  let earnings = [];
+
+
 
 
 //output DOM selectors
-let valueOutcome = document.querySelector("#totalValue");
+// let valueOutcome = document.getElementById("totalValue");
+
 let initialInvestment = document.querySelector("#initialInvestment");
 let earningsOutcome = document.querySelector("#earnings");
 let contributionsOutcome = document.querySelector("#contributions");
 
-const table =  document.getElementById("table");
+const table = document.getElementById("table");
 
 
 
@@ -47,7 +44,7 @@ document.querySelectorAll(".input__box--money").forEach((input) => {
   input.addEventListener("keyup", (event) => {
     if (event.keyCode === 13) {
       button.click();
-  };
+    };
   });
 });
 
@@ -64,20 +61,25 @@ interest.addEventListener("change", () => {
 
 let compound = function () {
 
-  results.classList.remove("component--hidden");
-  details.classList.remove("component--hidden");
-
-//remove existing rows from details section
-for (let i = document.querySelector(".table").rows.length; i > 2; i--) {
-document.querySelector(".table").deleteRow(i-1);
-}
+  document.querySelector(".outcomes").classList.add("open");
+  document.querySelector(".additional-info").classList.add("open");
 
 
- 
+  let years = [];
+  let initial = [];
+  let contributions = [];
+  let earnings = [];
+
+
+
+
+  //remove existing rows from details section
+  for (let i = document.querySelector(".table").rows.length; i > 2; i--) {
+    document.querySelector(".table").deleteRow(i - 1);
+  }
 
   let totalContributions = 0;
 
-  
   let total = [];
   let totalEarnings = 0;
 
@@ -101,7 +103,7 @@ document.querySelector(".table").deleteRow(i-1);
 
   let totalValue = p;
   let interestEarned = 0;
-  
+
 
 
   while (x < n) {
@@ -114,7 +116,7 @@ document.querySelector(".table").deleteRow(i-1);
     let contCol = row.insertCell(3);
     let endCol = row.insertCell(4);
 
-    //style rows
+    //style 
     yearCol.classList.add("table__item");
     startCol.classList.add("table__item");
     intCol.classList.add("table__item");
@@ -124,20 +126,20 @@ document.querySelector(".table").deleteRow(i-1);
 
     //Push initial value to the chart
     initial.push(p);
-     
+
     //Column 1: Year
     yearCol.innerHTML = currentYear;
 
     //Column 2: Starting Amount
-    startCol.innerHTML = formatCurrency(totalValue.toFixed(2));
+    startCol.innerHTML = formatCurrency(totalValue.toFixed(0));
 
-    interestEarned = parseFloat((totalValue * i).toFixed(2));
+    interestEarned = parseFloat((totalValue * i).toFixed(0));
 
     //Column 3: Interest Earned
     intCol.innerHTML = formatCurrency(interestEarned);
-    totalEarnings = parseFloat((interestEarned + totalEarnings).toFixed(2));
-  
-    totalValue = parseFloat((totalValue + (totalValue * i) + (c * f)).toFixed(2));
+    totalEarnings = parseFloat((interestEarned + totalEarnings).toFixed(0));
+
+    totalValue = parseFloat((totalValue + (totalValue * i) + (c * f)).toFixed(0));
 
 
     //Column 4: Contribution
@@ -150,33 +152,34 @@ document.querySelector(".table").deleteRow(i-1);
     totalContributions = c * f + totalContributions;
 
     earnings.push(totalEarnings);
- 
+
     contributions.push(totalContributions);
 
     total.push(parseFloat((totalEarnings + totalContributions + p).toFixed(2)));
-    
+
     years.push(currentYear);
     currentYear++;
     x++;
+    console.log(totalValue);
   }
 
 
-  document.querySelector("#yearsOutcome").innerHTML = ` after ${n} years`;
 
-  valueOutcome.innerHTML = formatCurrency(totalValue.toFixed(0));
-  earningsOutcome.innerHTML = formatCurrency(totalEarnings.toFixed(0));
-  contributionsOutcome.innerHTML = formatCurrency(totalContributions);
-  initialInvestment.innerHTML = formatCurrency(p);
+  document.getElementById("value-outcome").innerHTML = formatCurrency(totalValue.toFixed(0));
+  document.getElementById("interest-outcome").innerHTML = formatCurrency(totalEarnings.toFixed(0));
+  document.getElementById("contributions-outcome").innerHTML = formatCurrency(totalContributions.toFixed(0));
+  document.getElementById("years-outcome").innerHTML = `after ${n} years:`;
 
-  console.log(years,initial, contributions, earnings);
+
+  console.log(years, initial, contributions, earnings);
 
   chart.data.datasets[0].data = initial;
   chart.data.datasets[1].data = contributions;
   chart.data.datasets[2].data = earnings;
   chart.data.labels = years;
   chart.update();
-  
- 
+
+
 };
 
 button.addEventListener("click", compound);
@@ -187,7 +190,7 @@ button.addEventListener("click", compound);
 
 let ctx = document.getElementById("main-chart");
 Chart.defaults.global.defaultFontColor = "#2f394b";
-Chart.defaults.global.defaultFontFamily = "Helvetica";
+
 Chart.defaults.global.defaultFontStyle = "bold";
 
 Chart.scaleService.updateScaleDefaults("linear", {
@@ -195,9 +198,6 @@ Chart.scaleService.updateScaleDefaults("linear", {
     min: 0,
   },
 });
-
-
-
 
 let chart = new Chart(ctx, {
   // The type of chart we want to create
@@ -209,21 +209,21 @@ let chart = new Chart(ctx, {
     datasets: [{
         label: "Initial Investment",
         data: [],
-        backgroundColor: "#ce694f",
+        backgroundColor: "#113C6C",
         width: "100%",
-        fontSize: 16,
+
       },
       {
         label: "contributions",
         data: [],
-        backgroundColor: "#ee6d4d",
+        backgroundColor: "#2377D9",
         width: "100%",
-        fontSize: 16,
+        fontSize: 14,
       },
       {
         label: "Interest Earned",
         data: [],
-        backgroundColor: "#f5967f",
+        backgroundColor: "#A3C7F1",
         width: "100%",
         fontSize: 16,
       },
@@ -241,34 +241,32 @@ let chart = new Chart(ctx, {
     scales: {
       xAxes: [{
         gridLines: {
-                display:false
-            },
+          display: false
+        },
         scaleLabel: {
-        display: true,
-        fontFamily: 'Helvetica',
-        labelString: 'Year'
+          display: true,
+          labelString: 'Year'
         },
         stacked: true,
-        }],
-      
+      }],
+
       yAxes: [{
-        
-       
+
+
         stacked: true,
         ticks: {
+          maxTicksLimit: 6,
           beginAtZero: true,
+          fontSize: 16,
           callback: function (value, index, values) {
             if (parseInt(value) >= 1000) {
               return (
                 "$" + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
               );
             } else {
-              return "$" + value ;
+              return "$" + value;
             }
           },
-        fontColor: "#2f394b",
-      fontFamily: "Helvetica",
-      fontSize: 12,
         },
       }, ],
     },
@@ -276,7 +274,7 @@ let chart = new Chart(ctx, {
       display: false,
     },
     chart: {
-      
+
     },
   }
-  });
+});
